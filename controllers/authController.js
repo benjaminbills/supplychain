1054fb92b688b5a1616d52a1e0643d0de57668f6
Benjamin Obafemi <benjaminoba234@gmail.com>
2023-03-06 10:20:47 +0300

    add login

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,3 +22,33 @@ exports.signUp = async (req, res) => {
     });
   }
 };
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      res.status(404).json({
+        status: 'fail',
+        message: 'user not found',
+      });
+    }
+    const isCorrect = await bycrypt.compare(password, user.password);
+    if (isCorrect) {
+      console.log(req.sessions);
+      res.status(200).json({
+        status: 'success',
+      });
+    } else {
+      res.status(400).json({
+        status: 'fail',
+        message: 'incorrect username or password',
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+    });
+  }
+};
